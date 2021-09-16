a2b26566412346b6b7fa1f5b53c1c5bd90e341bf
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-09-16 12:00:26 +0900

    Fix wrong path

--- install-OracleDatabase21/install_sample.sh
+++ install-OracleDatabase21/install_sample.sh
@@ -13,5 +13,5 @@ curl -L# https://github.com/oracle/db-sample-schemas/archive/refs/tags/v21.1.tar
   | tar xzf - -C "$SCRIPT_DIR"
 cd "$SCRIPT_DIR"/db-sample-schemas-21.1
 perl -p -i.bak -e 's#__SUB__CWD__#'"$(pwd)"'#g' ./*.sql ./*/*.sql ./*/*.dat
-echo "@mksample $1 $1 $1 $1 $1 $1 $1 $1 users temp $SCRIPT_DIR/log $2" \
+echo "@mksample $1 $1 $1 $1 $1 $1 $1 $1 users temp $SCRIPT_DIR/log/ $2" \
   | sqlplus system/"$1"@"$2"
